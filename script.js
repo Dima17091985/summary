52fe24e749933d40a7e9e970580e4e814708be6c
mdd17091985@gmail.com <mdd17091985@gmail.com>
2023-11-29 21:32:49 +0300

    29.11.23

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
 var button = document.querySelector(".icons4");
 button.addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode");
-    var mainElement = document.querySelector(".main");
-    var profileImgElement = document.querySelector(".profile-img img");
+            document.body.classList.toggle("dark-mode");
+            var mainElement = document.querySelector("main");
+            var asideElement = document.querySelector("aside");
+            var profileImgElement = document.querySelector(".profile-img img");
 
-    if (document.body.classList.contains("dark-mode")) {
-        button.textContent = "wb_sunny";
+            if (document.body.classList.contains("dark-mode")) {
+                button.textContent = "wb_sunny";
+                mainElement.style.backgroundColor = "rgb(134, 134, 134)";
+                asideElement.style.backgroundColor = "rgb(134, 134, 134)";
+                profileImgElement.src = "image/profile2.jpg";
 
-        mainElement.style.backgroundColor = "rgb(50, 50, 50)";
-        mainElement.style.transition = "background-color 0.8s";
-        profileImgElement.src = "image/profile2.jpg";
-        profileImgElement.style.transition = "opacity 0.8s";
-        profileImgElement.style.opacity = "0";
-        setTimeout(function () {
-            profileImgElement.style.opacity = "1";
-        }, 100);
-    } else {
-        button.textContent = "nightlight";
+            } else {
+                button.textContent = "nightlight";
 
-        mainElement.style.backgroundColor = "rgb(255, 255, 255)";
-        mainElement.style.transition = "background-color 0.8s";
-        profileImgElement.style.transition = "opacity 0.8s";
-        profileImgElement.style.opacity = "0";
-        setTimeout(function () {
-            profileImgElement.src = "image/profile.jpg";
-            profileImgElement.style.opacity = "1";
-        }, 100);
-    }
-});
+                mainElement.style.backgroundColor = "rgb(255, 255, 255)";
+                asideElement.style.backgroundColor = "rgb(255, 255, 255)";
+                profileImgElement.src = "image/profile.jpg";
+            }
+        });
+        
